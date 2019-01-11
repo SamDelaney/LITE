1a371a238f98c8f482cd8490855aa6e12300e2eb
Sam Delaney <sam@thedelaneys.net>
2019-01-11 13:09:11 -0800

    Added model support for language name options.

--- FormattedExample.py
+++ FormattedExample.py
@@ -2,6 +2,7 @@ from FormatStyle import FormatStyle
 
 class FormattedExample:
     selectedFormatStyle = FormatStyle("3-line GSRL standard")
+    langNameOption = 0
     pastedText = ""
     
     def __init__(self, text, formatStyle):
--- LITE.py
+++ LITE.py
@@ -1,11 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from LITE_UI import Ui_LITE
+#from ExampleDisplayWidget import Ui_ExampleWidget
 from FormatStyle import FormatStyle
 from FormattedExample import FormattedExample
 from ScriptManager import ScriptManager
 import clipboard
 
 formatStyles = []
+exampleHistory = []
 sm = ScriptManager()
 currentExample = FormattedExample("", FormatStyle("")) #initialized with empty values
 
@@ -14,10 +16,24 @@ def initialize_globals():
     formatStyles.append(FormatStyle("3-line GSRL standard"))
     formatStyles.append(FormatStyle("4-line GSRL standard"))
     currentExample.selectedFormatStyle = formatStyles[0]
+    #ui.exampleListView.setModel(Ui_ExampleWidget())
 
-def refresh_ui():
+#returns 0 if no language name
+#returns 1 if include language name as first line
+#returns 2 if include language name on right side
+def getLangNameOption():
+    if ui.noLangNameButton.isChecked():
+        currentExample.langNameOption = 0
+    if ui.langNameFirstLineButton.isChecked():
+        currentExample.langNameOption = 1
+    if ui.langNameOnRightButton.isChecked():
+        currentExample.langNameOption = 2
+    refresh_ui()
+
+def refresh_ui():    
     for fs in formatStyles:
         ui.formatStyleComboBox.addItem(fs.stylename)
+
     ui.outputPreviewTextEdit.setText(sm.convert_text(currentExample))
 
 def retrieve_clipboard():
@@ -27,10 +43,17 @@ def retrieve_clipboard():
 
 def copy_to_clipboard():
     clipboard.copy(currentExample.pastedText)
+    exampleHistory.append(currentExample)
 
 def set_event_connections():
     ui.pasteButton.clicked.connect(lambda: retrieve_clipboard())
     ui.copyButton.clicked.connect(lambda: copy_to_clipboard())
+
+    #language name option radio buttons
+    ui.noLangNameButton.clicked.connect(lambda: getLangNameOption())
+    ui.langNameFirstLineButton.clicked.connect(lambda: getLangNameOption())
+    ui.langNameOnRightButton.clicked.connect(lambda: getLangNameOption())
+    
 
 if __name__ ==  "__main__":
     import sys
